ad3b8fcebdf89e6e9cd504770ae7f95c8f418f74
tmofveins <nicksiow.zj@gmail.com>
2023-02-13 22:22:29 +0800

    converted first test img to grayscale

--- ocr/ocr.py
+++ ocr/ocr.py
@@ -2,6 +2,5 @@ import cv2
 import pytesseract
 import numpy as np
 
-img = cv2.imread("ocr/c1ocr_testing/image.png")
-cv2.imshow("img", img)
-cv2.waitKey(0)
+img = cv2.imread("ocr/c1ocr_testing/image.png", 0)
+cv2.imwrite("test.png", img)
